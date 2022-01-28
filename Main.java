5086df3fd3bcf1c023ec15bf43baa2aa2062ed2d
Vladimir Levin <94986766+francLv@users.noreply.github.com>
2022-01-28 16:29:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        scanner.close();
+    }
+}
